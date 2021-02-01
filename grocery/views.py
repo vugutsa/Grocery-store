08474e122999252c6fd585db7596fe71368b19d1
vugutsa <vugutsamercy84@gmail.com>
2021-02-01 22:05:24 +0300

    create the convert dates view function

--- grocery/views.py
+++ grocery/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
+import datetime as dt
 # Create your views here.
 
 def welcome(request):
     return HttpResponse('Hey there,Welcome!')
+
+def grocery_today(request):
+    date = dt.date.today()
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+
+def convert_dates(dates):
+    
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
--- store/urls.py
+++ store/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^grocery/',include('grocery.urls'))
+    path(r'grocery/',include('grocery.urls')),
 ]
