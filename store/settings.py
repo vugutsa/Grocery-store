55818a30d8fab5ad5dcb21c4c1ff52454c0ff1e3
vugutsa <vugutsamercy84@gmail.com>
2021-02-02 08:54:43 +0300

    create a database called grocery

--- store/settings.py
+++ store/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'grocery',
+        'USER': 'moringa',
+    'PASSWORD':'Access',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
