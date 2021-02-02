b53db4fba2bddcd28c513cb28ed9560bea05b7ee
vugutsa <vugutsamercy84@gmail.com>
2021-02-02 08:51:59 +0300

    create a navbar

--- grocery/views.py
+++ grocery/views.py
@@ -1,22 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse('Hey there,Welcome!')
-
+    return render(request, 'welcome.html')
 def grocery_today(request):
     date = dt.date.today()
     day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+                <h1>Grocery bought on {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
-
+    return render(request, 'all-grocery/today_grocery.html', {"date": date,})
 def convert_dates(dates):
     
     # Function that gets the weekday number for the date.
@@ -27,3 +26,19 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
+def past_days_grocery(request, past_date):
+    
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-grocery/past-news.html', {"date": date})
