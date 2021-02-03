3654a414214e06c52fd4c4e2a46829d1b8277e24
vugutsa <vugutsamercy84@gmail.com>
2021-02-03 14:23:04 +0300

    add a register form in the forms.py

--- grocery/forms.py
+++ grocery/forms.py
@@ -1,13 +1,13 @@
-# from django import forms
-# from django.contrib.auth.models import User
-# from django.contrib.auth.forms import UserCreationForm
-# from .models import *
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
 
 
 
-# class RegisterForm(UserCreationForm):
-#     email = forms.EmailField()
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
 
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password1', 'password2']
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
